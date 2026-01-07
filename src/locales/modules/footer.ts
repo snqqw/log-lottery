12d363f5f4d39680c09308c700b4812e609ca5ae
abby <610065207@qq.com>
2026-01-07 21:55:21 +0800

    docs: 微调

--- src/locales/modules/footer.ts
+++ src/locales/modules/footer.ts
@@ -1,10 +1,10 @@
 export const footerEn = {
-    'self-reflection': "{'@'}云眠AI版权所有",
+    'self-reflection': "{'@'}云眠AI工作室版权所有",
     'thiefEasy': '',
 }
 
 export const footerZhCn = {
-    'self-reflection': "{'@'}云眠AI版权所有",
+    'self-reflection': "{'@'}云眠AI工作室版权所有",
     'thiefEasy': '破山中贼易，破心中贼难',
 }
 
